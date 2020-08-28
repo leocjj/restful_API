5e6b6e9ff61f3c3cb9202c455d94b567f708ee66
Leonardo Calderon J <leocj@hotmail.com>
2020-08-27 22:46:36 -0500

    CRUD methods implemented
    End points for GET, POST, PUT and DELETE.
    http://localhost:5000/GET
    http://localhost:5000/POST
    http://localhost:5000/PUT
    http://localhost:5000/DELETE

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #coding: utf-8
 """
 Start a Flask web application on port 5000
+https://flask.palletsprojects.com/en/0.12.x/api/#url-route-registrations
 """
 
 from flask import Flask
@@ -8,10 +9,31 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
-def hello():
+def index():
     """returns general response message"""
     return render_template('index.html')
 
+@app.route('/GET', methods=['GET'])
+def get():
+    """return to GET request message"""
+    return "Get response"
+
+@app.route('/POST', methods=['POST'])
+def post():
+    """return to POST request message"""
+    return "Post response"
+
+@app.route('/PUT', methods=['PUT'])
+def put():
+    """return to PUT request message"""
+    return "Put response"
+
+@app.route('/DELETE', methods=['DELETE'])
+def delete():
+    """return to DELETE request message"""
+    return "Delete response"
+
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port='5000')
+    app.run(debug=True, host='0.0.0.0', port='5000')
